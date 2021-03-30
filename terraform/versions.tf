9a974d27544a17e44e9a1ab209b3c3094100b849
Aleksei Shibanov <ag.shibanov@gmail.com>
2021-03-30 18:42:47 +0300

    Fix versions.tf 9 .0

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,7 @@
 terraform {
+  required_version = ">= 0.12"
   required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.35"
-    
-    }
+    yandex = "~> 0.35"
 
   }
 
