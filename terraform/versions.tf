623a7508525ba772f6aeaed185fb8e2b2f978e1e
geckonoff <79303327+geckonoff@users.noreply.github.com>
2021-03-30 18:22:47 +0300

    Fix versions.tf 5.0

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    provider "yandex" = {
+    provider "yandex" {
       source  = "yandex-cloud/yandex"
       version = ">= 0.35.0"
     
